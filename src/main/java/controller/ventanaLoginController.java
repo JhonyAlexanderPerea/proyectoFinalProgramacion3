bd88722ac144c0b6e87093f7de622f9c5f134c18
JhonyAlexanderPerea <106632266+JhonyAlexanderPerea@users.noreply.github.com>
2024-04-06 22:40:51 -0500

    Actualización del diseño de ventanaLogin.fxml

--- src/main/java/controller/ventanaLoginController.java
+++ src/main/java/controller/ventanaLoginController.java
@@ -1,4 +1,32 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class ventanaLoginController {
+
+    @FXML
+    private Button btnCrearCuenta;
+
+    @FXML
+    private Button btnIngresar;
+
+    @FXML
+    private Label lblContrasenia;
+
+    @FXML
+    private Label lblLogin;
+
+    @FXML
+    private Label lblUsuario;
+
+    @FXML
+    private PasswordField txtContrasenia;
+
+    @FXML
+    private TextField txtUsuario;
+
 }

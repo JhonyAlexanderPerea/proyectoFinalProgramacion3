8edb0ff4a44941c88c0a01c7cce41c22d0aa518c
JhonyAlexanderPerea <106632266+JhonyAlexanderPerea@users.noreply.github.com>
2024-04-13 21:41:10 -0500

    se modifica la clase GduqExceptions

--- src/main/java/repository/csvdao/PublicadorCsvDao.java
+++ src/main/java/repository/csvdao/PublicadorCsvDao.java
@@ -1,4 +1,4 @@
-ackage repository.csvdao;
+package repository.csvdao;
 
 import java.io.BufferedReader;
 import java.io.Closeable;
@@ -6,7 +6,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Formatter;
-import domain.Publicador;
+import model.Publicador;
 import exception.GduqExceptions;
 import exception.GduqExceptions.CsvDePublicadorMalFormado;
 import exception.GduqExceptions.PublicadorDuplicadoException;

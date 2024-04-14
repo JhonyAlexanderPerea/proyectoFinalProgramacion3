f5f41755a1ced1cf57c1110da8a8d2ac777ef5c1
FabianValencia23 <127154183+FabestValencia@users.noreply.github.com>
2024-04-13 20:51:01 -0500

    Se modifico la clase contenido

--- src/main/java/model/Contenido.java
+++ src/main/java/model/Contenido.java
@@ -1,4 +1,90 @@
 package model;
 
+import java.time.OffsetDateTime;
+import java.time.ZonedDateTime;
+import java.util.Objects;
+
 public class Contenido {
+
+    protected long idContenido;
+    protected String titulo;
+    protected String fechaPublicacion;
+    protected OffsetDateTime fechaPublicacionOdt;
+    protected ZonedDateTime fechaPublicacionZdt;
+
+    public Contenido(long idContenido, String titulo, String fechaPublicacion) {
+        this.idContenido = idContenido;
+        this.titulo = titulo;
+        this.fechaPublicacion = fechaPublicacion;
+    }
+
+    public Contenido(long idContenido, String titulo, OffsetDateTime fechaPublicacionOdt) {
+        this.idContenido = idContenido;
+        this.titulo = titulo;
+        this.fechaPublicacionOdt = fechaPublicacionOdt;
+    }
+
+    public Contenido(long idContenido, String titulo, ZonedDateTime fechaPublicacionZdt) {
+        this.idContenido = idContenido;
+        this.titulo = titulo;
+        this.fechaPublicacionZdt = fechaPublicacionZdt;
+    }
+
+    public long getIdContenido() {
+        return idContenido;
+    }
+
+    public void setIdContenido(long idContenido) {
+        this.idContenido = idContenido;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getFechaPublicacion() {
+        return fechaPublicacion;
+    }
+
+    public void setFechaPublicacion(String fechaPublicacion) {
+        this.fechaPublicacion = fechaPublicacion;
+    }
+
+    public OffsetDateTime getFechaPublicacionOdt() {
+        return fechaPublicacionOdt;
+    }
+
+    public void setFechaPublicacionOdt(OffsetDateTime fechaPublicacionOdt) {
+        this.fechaPublicacionOdt = fechaPublicacionOdt;
+    }
+
+    public ZonedDateTime getFechaPublicacionZdt() {
+        return fechaPublicacionZdt;
+    }
+
+    public void setFechaPublicacionZdt(ZonedDateTime fechaPublicacionZdt) {
+        this.fechaPublicacionZdt = fechaPublicacionZdt;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Contenido other = (Contenido) obj;
+        return Objects.equals(fechaPublicacion, other.fechaPublicacion) && idContenido == other.idContenido
+                && Objects.equals(titulo, other.titulo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idContenido, titulo, fechaPublicacion);
+    }
 }

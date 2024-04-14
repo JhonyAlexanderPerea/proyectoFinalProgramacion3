478da68af656e457d19b8e5d3524898c0c2d26a9
FabianValencia23 <127154183+FabestValencia@users.noreply.github.com>
2024-04-13 21:21:32 -0500

    Se modifico la clase Foto

--- src/main/java/model/Foto.java
+++ src/main/java/model/Foto.java
@@ -1,4 +1,23 @@
 package model;
 
-public class Foto {
+public class Foto extends Contenido{
+
+    byte[] cuerpo;
+
+    public Foto(long idContenido, String titulo, String fechaPublicacion) {
+        super(idContenido, titulo, fechaPublicacion);
+    }
+
+    public Foto(long idContenido, String titulo, String fechaPublicacion, byte[] cuerpo) {
+        super(idContenido, titulo, fechaPublicacion);
+        this.cuerpo = cuerpo;
+    }
+
+    public byte[] getCuerpo() {
+        return cuerpo;
+    }
+
+    public void setCuerpo(byte[] cuerpo) {
+        this.cuerpo = cuerpo;
+    }
 }

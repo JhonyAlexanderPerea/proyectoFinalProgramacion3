86896100175369a51fe4ba066dde484477008ed0
JhonyAlexanderPerea <106632266+JhonyAlexanderPerea@users.noreply.github.com>
2024-04-14 18:46:05 -0500

    funcionalidad de carga de archivos

--- src/main/java/Networking/clienteArchivos.java
+++ src/main/java/Networking/clienteArchivos.java
@@ -1,6 +1,7 @@
 package Networking;
 
 import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 public class clienteArchivos {
@@ -10,78 +11,39 @@ public class clienteArchivos {
     private DataInputStream flujoEntrada;
     private DataOutputStream flujoSalida;
     private int puerto;
+    private ServerSocket escuchadorDePeticiones;
 
     public clienteArchivos(String direccionIP, int puerto) {
         this.direccionIP = direccionIP;
         this.puerto = puerto;
     }
 
-    public boolean iniciarConexionConServidor() {
-        try {
-            this.conector 		= new Socket(this.direccionIP, this.puerto);
-            this.flujoEntrada 	= new DataInputStream(this.conector.getInputStream());
-            this.flujoSalida 	= new DataOutputStream(conector.getOutputStream());
+    public void enviarArchivo(File archivo) {
+        String host = "localhost";
+        int puerto = 10;
 
-            return true;
-        } catch(Exception e) {
-            return false;
-        } finally {
-            // TODO
-        }
-    }
-    public void enviarArchivo(String rutaArchivo) {
-        try {
-            // Abrir el archivo
-            FileInputStream fileInputStream = new FileInputStream(rutaArchivo);
-            BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
-
-            // Obtener el tamaño del archivo
-            long tamanoArchivo = fileInputStream.available();
-
-            // Enviar el nombre del archivo y su tamaño al servidor
-            flujoSalida.writeUTF(new File(rutaArchivo).getName());
-            flujoSalida.writeLong(tamanoArchivo);
-
-            // Crear un buffer para leer el archivo en bloques
+        try (Socket socket = new Socket(host, puerto)) {
             byte[] buffer = new byte[4096];
-            int bytesRead;
-            while ((bytesRead = bufferedInputStream.read(buffer)) != -1) {
-                // Escribir los bytes en el flujo de salida
-                flujoSalida.write(buffer, 0, bytesRead);
+
+            // Enviar el nombre del archivo al servidor
+            ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
+            outputStream.writeObject(archivo.getName());
+
+            // Enviar el contenido del archivo al servidor
+            try (FileInputStream fileInputStream = new FileInputStream(archivo)) {
+                int bytesRead;
+                while ((bytesRead = fileInputStream.read(buffer)) != -1) {
+                    socket.getOutputStream().write(buffer, 0, bytesRead);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
 
-            // Cerrar recursos
-            bufferedInputStream.close();
-            fileInputStream.close();
+            System.out.println("Archivo enviado correctamente al servidor.");
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void cerrarRecursos() {
-        try {
-            this.flujoEntrada.close();
-            this.flujoSalida.close();
-            this.conector.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-
-    public DataInputStream getFlujoEntrada() {
-        return flujoEntrada;
-    }
-
-    public void setFlujoEntrada(DataInputStream flujoEntrada) {
-        this.flujoEntrada = flujoEntrada;
-    }
-
-    public DataOutputStream getFlujoSalida() {
-        return flujoSalida;
-    }
-
-    public void setFlujoSalida(DataOutputStream flujoSalida) {
-        this.flujoSalida = flujoSalida;
-    }
 }

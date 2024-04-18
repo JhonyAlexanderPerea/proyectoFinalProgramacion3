9bdef93eab322c8e2a4871509d2d7c386c1f19bb
JhonyAlexanderPerea <106632266+JhonyAlexanderPerea@users.noreply.github.com>
2024-04-18 00:30:15 -0500

    correcion

--- src/main/java/Networking/ServerMain.java
+++ src/main/java/Networking/ServerMain.java
@@ -4,28 +4,28 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+
 public class ServerMain {
-	private static boolean serverRunning = true;
 
 	public static void montar() {
 		int port = 10;
+		//int hilosMaximos = 5;
 
+		//ExecutorService executor = Executors.newFixedThreadPool(hilosMaximos);
+		//UtilsJPA.getEntityManager();
 		try (ServerSocket server = new ServerSocket(port)) {
 			System.out.println("Esperando peticiones...");
-			while (serverRunning) {
+			while (true) {
 				Socket socket = server.accept();
 				System.out.println("cliente conectado");
 
 				ClienteHilo hilo = new ClienteHilo(socket);
 				Thread hiloThread = new Thread(hilo);
 				hiloThread.start();
+
 			}
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
-	}
-
-	public static void detenerServidor() {
-		serverRunning = false;
 	}
 }
--- src/main/java/controller/ventanaPublicacionesController.java
+++ src/main/java/controller/ventanaPublicacionesController.java
@@ -12,19 +12,10 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-
 import java.io.File;
 import java.io.IOException;
-import java.net.ServerSocket;
 
 public class ventanaPublicacionesController {
-
-    private App app;
-    private ventanaLoginController ventanaLoginController;
-    private Stage stage1;
-
-    private Thread hiloServer;
-    private ServerSocket serverSocket;
 
     @FXML
     private Button btnArchivos;
@@ -37,6 +28,13 @@ public class ventanaPublicacionesController {
 
     @FXML
     private Label lblArchivoSeleccionado;
+
+    private App app;
+
+    private ventanaLoginController ventanaLoginController;
+
+    private Stage stage1;
+
 
     @FXML
     void escogerArchivo(ActionEvent event) {
@@ -51,11 +49,13 @@ public class ventanaPublicacionesController {
         }
     }
 
+
     @FXML
     void subirArchivo(ActionEvent event) {
         // Verificar si se ha seleccionado un archivo
         String nombreArchivo = lblArchivoSeleccionado.getText();
-        if (nombreArchivo.isEmpty()) {
+        if (nombreArchivo.isEmpty())
+        {
             System.out.println("No se ha seleccionado ningún archivo.");
             return;
         }
@@ -64,6 +64,7 @@ public class ventanaPublicacionesController {
 
         String direccionIP = "127.0.0.1"; // Dirección IP del servidor
         int puerto = 10; // Puerto del servidor
+
 
         // Crear cliente de archivos y establecer la conexión con el servidor
         clienteArchivos cliente = new clienteArchivos(direccionIP, puerto);
@@ -78,46 +79,28 @@ public class ventanaPublicacionesController {
             System.err.println("Error al enviar el archivo: " + e.getMessage());
         }
     }
-
     @FXML
-    void montarServidor(ActionEvent actionEvent) {
-        hiloServer = new Thread(() -> {
-            try {
-                serverSocket = new ServerSocket(10); // Puerto del servidor
-                System.out.println("Esperando peticiones...");
-                while (true) {
-                    serverSocket.accept();
-                    System.out.println("cliente conectado");
-                }
-            } catch (IOException e) {
-                System.out.println("Servidor cerrado");
-            }
-        });
+    public void montarServidor(ActionEvent actionEvent) {
+        Thread hiloServer = new Thread( () -> ServerMain.montar());
         hiloServer.start();
     }
-
     @FXML
-    void abrirViewLogin(ActionEvent actionEvent) throws IOException {
-        if (hiloServer != null && hiloServer.isAlive()) {
-            try {
-                serverSocket.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        FXMLLoader loader = new FXMLLoader();
+    public void abrirViewLogin(ActionEvent actionEvent) throws IOException {
+        FXMLLoader loader= new FXMLLoader();
         loader.setLocation(App.class.getResource("/view/ventanaLogin.fxml"));
-        AnchorPane anchorPane = loader.load();
+        AnchorPane anchorPane= (AnchorPane)loader.load();
         ventanaLoginController loginController = loader.getController();
         loginController.setAplicacion(app);
-        Scene scene = new Scene(anchorPane);
+        Scene scene= new Scene(anchorPane);
         Stage stage = new Stage();
         stage.setScene(scene);
         stage.setTitle("LOGIN");
         loginController.initPublicaciones(stage, this);
         stage.show();
         this.stage1.close();
+    }
+
+    private void init(Stage stage, ventanaPublicacionesController ventanaPublicacionesController) {
     }
 
     public void setApp(App app) {

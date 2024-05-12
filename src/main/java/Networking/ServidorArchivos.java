63ed01fa19b2ff93713f46e1a30473110a54b828
JhonyAlexanderPerea <106632266+JhonyAlexanderPerea@users.noreply.github.com>
2024-05-11 22:23:35 -0500

    actualizacion

--- src/main/java/Networking/ServidorArchivos.java
+++ src/main/java/Networking/ServidorArchivos.java
@@ -7,7 +7,7 @@ import java.net.Socket;
 public class ServidorArchivos {
 
     private int puerto;
-    private String rutaDirectorio = "archivos/";
+    private String rutaDirectorio = "src/main/archivos/";
 
     public ServidorArchivos(int puerto) {
         this.puerto = puerto;
